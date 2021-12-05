245e985408b9bd92618429b84d38de04441f9bbf
Kaushal-Suresh <viji.sur@gmail.com>
2021-12-05 19:27:43 +0530

    first commit

---
+++ src/Alert.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Alert() {
+    return (
+        <div>
+            <section className="alert-section top-50">
+                <div className="container">
+                    <div className="alert-text">
+                        Shop now at the Apple Store online with many great ways to buy. Get free, no-contact delivery, specialist help and more.
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Alert
